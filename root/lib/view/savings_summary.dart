5eea6d08196301b506a23958645f5ca945d40c53
Cjking57893 <cody.king04@outlook.com>
2025-10-26 01:06:15 -0400

    Savings section icon added to transactions summary screen

---
+++ root/lib/view/savings_summary.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SavingsSummaryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Savings")
+      )
+    );
+  }
+}
